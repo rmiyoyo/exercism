bc11827577cdef81d5e927c37c2f92d8edc2c14f
rmiyoyo <miyoyoraphael@gmail.com>
2025-01-18 23:11:20 +0300

    Add annalyns-infiltration exercise

---
+++ javascript/annalyns-infiltration/annalyns-infiltration.js
@@ -0,0 +1,20 @@
+export function canExecuteFastAttack(knightIsAwake) {
+  throw new Error('Remove this line and implement the function');
+}
+
+export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
+  throw new Error('Remove this line and implement the function');
+}
+
+export function canSignalPrisoner(archerIsAwake, prisonerIsAwake) {
+  throw new Error('Remove this line and implement the function');
+}
+
+export function canFreePrisoner(
+  knightIsAwake,
+  archerIsAwake,
+  prisonerIsAwake,
+  petDogIsPresent,
+) {
+  throw new Error('Remove this line and implement the function');
+}
